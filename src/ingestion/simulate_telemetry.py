9defba82dc1cf436b33160bc8aa5f5879b9b6ef9
jgamboa17 <jgamboa0117@gmail.com>
2025-04-27 22:43:06 -0700

    Updated simulate_telemetry.py with csv file I/O and will implement PostgreSQL with the python code

--- src/ingestion/simulate_telemetry.py
+++ src/ingestion/simulate_telemetry.py
@@ -4,21 +4,49 @@
 
 import random
 import time
+from datetime import datetime
+import csv
+import os
+import subprocess
 
 def generate_random_telemetry():
     telemetry_data = {
-        "timestamp": time.time(),
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         "latitude": random.uniform(-90, 90),
-        "longitude": random.uniform(-180, 180),
-        "altitude_m": random.uniform(0, 40000), # meters
-        "velocity_m_s": random.uniform(0, 8000), # meters per second
+        "longitude": random.uniform(-180.0, 180.0),
+        "altitude_m": random.uniform(160.0, 40000.0), # meters
+        "velocity_m_s": 7800 + random.uniform(-50.0,50.0), # meters per second
+        "satellite_id" : random.uniform(0, 10.0), # randrom satellite ID, once data is implemented, will have a real ID
+        "battery_level_percent" : random.uniform(20.0,100.0),
+        "temperature_celcius": random.uniform(-100.0, 100,0),
+        "system_status": random.choice(["Nominal", "Warning", "Critical"])
+
     }
     return telemetry_data
+
+def save_to_csv(data, filename="telementry_log.csv"):
+    file_exists = os.path.isfile(filename)
+
+    with open(filename, mode="a", newline="") as csvfile:
+        fieldnames = ["timestamp", "latitude", "longitude", "altitude_m", "velocity_m_s", "satellite_id", 
+        "battery_level_percent", "temperature_celcius", "system_status"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        if not file_exists:
+            writer.writeheader()
+
+        writer.writerow(data)
+
+        if not file_exists: 
+             subprocess.run(["code", filename])
+        
+    print("Saving to:", os.path.abspath(filename))
 
 def main():
     while True:
             data = generate_random_telemetry()
             print(data)
+            save_to_csv(data)
             time.sleep(1) #waits 1 second before updating information
 
 if __name__ == "__main__":
